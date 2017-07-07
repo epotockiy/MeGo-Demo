04ec197a2d2c4e53f4df37e1e48de3778fd533c4
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-07-07 13:58:39 +0300

    Added concat for styles and Clean task in gulpfile. Added basic html markup for footer.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var concat = require('gulp-concat');
+var clean  = require('gulp-clean');
+
+gulp.task('clean', function() {
+	return gulp.src('build/', { read: false })
+		.pipe(clean());
+})
 
 gulp.task('html', function() {
   return gulp.src('src/*.html')
@@ -14,15 +21,16 @@ gulp.task('images', function() {
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
+    .pipe(concat('styles.css'))
     .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('watch', ['html', 'images', 'sass'], function() {
+gulp.task('watch', ['clean', 'html', 'images', 'sass'], function() {
   console.log('changed');
   gulp.watch('src/*.html', ['html']);
   gulp.watch('src/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('build', ['html', 'images', 'sass']);
+gulp.task('build', ['clean', 'html', 'images', 'sass']);
 
-gulp.task('default', ['html', 'images', 'sass', 'watch']);
+gulp.task('default', ['build', 'watch']);
