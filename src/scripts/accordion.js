d8dae83c64a3611265e021dec6a72aee89847889
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-07-13 12:36:25 +0300

    Added work component. Fixed accordion in service-2 component.

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -2,13 +2,22 @@ let accordionBtns = document.getElementsByClassName('accordion-btn');
 
 for(let i = 0; i < accordionBtns.length; ++i) {
 	accordionBtns[i].onclick = function() {
-		this.classList.toggle('active');
 		let panel = this.nextElementSibling;
 
 		if(panel.style.maxHeight) {
 			panel.style.maxHeight = null;
 			panel.style.margin = 0;
 		} else {
+			for(let j = 0; j < accordionBtns.length; ++j) {
+				accordionBtns[j].classList.remove('active-btn');
+				let nextPanel = accordionBtns[j].nextElementSibling;
+				console.log(nextPanel);
+
+				nextPanel.style.maxHeight = null
+				nextPanel.style.margin = 0;
+			}
+
+			this.classList.toggle('active-btn');
 			panel.style.maxHeight = '146px';
 			panel.style.margin = '15px 25px 20px 20px';
 		}
