85d31f6481149ff81f543e7c5b16f0d9a4b2041d
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-07-19 12:32:13 +0300

    Fixed bugs with accordion in service-2 section. Rewrote scripts on pure JS.

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,24 +1,24 @@
 let accordionBtns    = document.getElementsByClassName('accordion-btn');
-let simplebarContent = document.getElementsByClassName('simplebar-content');
+let accordionItems   = document.getElementsByClassName('accordion-item');
 let accordionPanels  = document.getElementsByClassName('panel');
+let simplebarContent = document.getElementsByClassName('simplebar-content');
 
-(function() {
-	new SimpleBar(accordionPanels[0], {
-		autoHide: true
-	});
-	new SimpleBar(accordionPanels[1], {
-		autoHide: true
-	});
-	new SimpleBar(accordionPanels[2], {
-		autoHide: true
-	}).recalculate();
+new SimpleBar(accordionPanels[0], {
+	autoHide: true
+});
+new SimpleBar(accordionPanels[1], {
+	autoHide: true
+});
+new SimpleBar(accordionPanels[2], {
+	autoHide: true
+});
 
-	accordionBtns[0].nextElementSibling.style.maxHeight = '146px';
-	accordionBtns[0].nextElementSibling.style.margin = '15px 25px 20px 20px';
-	accordionBtns[0].classList.toggle('active-btn');
-})();
+accordionItems[0].style.borderBottom = '1px solid #e5e5e5';
+accordionBtns[0].nextElementSibling.style.maxHeight = '146px';
+accordionBtns[0].nextElementSibling.style.margin = '15px 25px 19px 20px';
+accordionBtns[0].classList.toggle('active-btn');
 
-for(let i = 0; i < accordionBtns.length; ++i) {
+for(let i = 0; i < accordionItems.length; ++i) {
 	accordionBtns[i].onclick = function() {
 		let panel = this.nextElementSibling;
 
@@ -26,6 +26,7 @@ for(let i = 0; i < accordionBtns.length; ++i) {
 			panel.style.maxHeight = null;
 			panel.style.margin = '0 25px 0 20px';
 			this.classList.toggle('active-btn');
+			accordionItems[i].style.borderBottom = 'none';
 		} else {
 			for(let j = 0; j < accordionBtns.length; ++j) {
 				accordionBtns[j].classList.remove('active-btn');
@@ -33,11 +34,13 @@ for(let i = 0; i < accordionBtns.length; ++i) {
 
 				nextPanel.style.maxHeight = null;
 				nextPanel.style.margin = '0 25px 0 20px';
+				accordionItems[j].style.borderBottom = 'none';
 			}
 
 			this.classList.toggle('active-btn');
 			panel.style.maxHeight = '146px';
-			panel.style.margin = '15px 25px 20px 20px';
+			panel.style.margin = '15px 25px 19px 20px';
+			accordionItems[i].style.borderBottom = '1px solid #e5e5e5';
 		}
 	}
 }
