36e17c3ee008dd80d064e03bf198874d77164f3a
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-07-19 21:57:39 +0300

    Added edit functionality and styling.

--- task2/build/scripts/scripts.js
+++ task2/build/scripts/scripts.js
@@ -1,83 +1,90 @@
 'use strict';
 
 (function($, undefined) {
-	var $addBtn        = $('.add-btn');
-	var $taskInput     = $('.task-input'); 
-	var $todoList      = $('.todo-list');
-	var tasksArray     = ['first'];
+  var $todoSection   = $('.todo');
+  var $addBtn        = $('.add-btn');
+  var $taskInput     = $('.task-input');
+  var $todoList      = $('.todo-list');
+  var tasksArray     = ['first'];
 
-	function renderTasksList() {
-		$todoList.empty();
+  function renderTasksList() {
+    $todoList.empty();
 
-		for(var i = 0; i < tasksArray.length; i++) {
-			var taskBlock = $('<div />', {
-				class: 'task'	
-			});
-			var editBtn = $('<input />', {
-				type: 'button',
-				value: 'Edit',
-				class: 'edit-btn'
-			});
-			var removeBtn = $('<input />', {
-				type: 'button',
-				value: 'X',
-				class: 'remove-btn'
-			});
-			var taskText = $('<span />', {
-				text: tasksArray[i]
-			});
+    for(var i = 0; i < tasksArray.length; i++) {
+      var taskBlock = $('<div />', {
+        class: 'task'
+      });
+      var editBtn = $('<input />', {
+        type: 'button',
+        value: 'Edit',
+        class: 'edit-btn'
+      });
+      var removeBtn = $('<input />', {
+        type: 'button',
+        value: 'X',
+        class: 'remove-btn'
+      });
+      var taskText = $('<span />', {
+        text: tasksArray[i]
+      });
 
-			setRemoveAction(removeBtn, i);
-			setEditAction(editBtn, i);
+      setRemoveAction(removeBtn, i);
+      setEditAction(editBtn, i);
 
-			taskBlock.append(taskText);
-			taskBlock.append(editBtn);
-			taskBlock.append(removeBtn);
-			$todoList.append(taskBlock);
-		}
-	}
+      taskBlock.append(taskText);
+      taskBlock.append(removeBtn);
+      taskBlock.append(editBtn);
+      $todoList.append(taskBlock);
+    }
+  }
 
-	function onAddBtnClick() {
-		tasksArray.push($taskInput.val());
-		$taskInput.val('');
-		renderTasksList();
-	}
+  function onAddBtnClick() {
+    tasksArray.push($taskInput.val());
+    $taskInput.val('');
+    renderTasksList();
+  }
 
-	function setRemoveAction(removeButton, index) {
-		removeButton.on('click', function() {
-			tasksArray.splice(index, 1);
-			renderTasksList();
-		});
-	}
+  function setRemoveAction(removeButton, index) {
+    removeButton.on('click', function() {
+      tasksArray.splice(index, 1);
+      renderTasksList();
+    });
+  }
 
-	function setEditAction(editButton, index) {
-		var editBlock = $('<div />', {
-			class: 'edit'
-		});
-		var editInput = $('<input />', {
-			type: 'text',
-			value: tasksArray[index],
-			class: 'edit-input'
-		});
-		var saveButton = $('<input />', {
-			type: 'button',
-			value: 'Save',
-			class: 'edit-btn'
-		});
-		editBlock.append(editInput);
-		editBlock.append(saveButton);
+  function setEditAction(editButton, index) {
+    editButton.on('click', function() {
+      var editBlock = $('<div />', {
+        class: 'edit'
+      });
+      var editInput = $('<input />', {
+        type: 'text',
+        value: tasksArray[index],
+        class: 'edit-input'
+      });
+      var saveButton = $('<input />', {
+        type: 'button',
+        value: 'Save',
+        class: 'save-btn'
+      });
+      editBlock.append(editInput);
+      editBlock.append(saveButton);
 
-		editButton.on('click', setOpenEditDialogAction(editBlock));
-	}
+      $todoSection.append(editBlock);
 
-	function setOpenEditDialogAction(block) {
-		console.log("Here");
-		console.log(block);
-		// block.style.display = 'block';
-	}
+      setSaveAction(saveButton, editInput, index, editBlock);
+    });
+  }
 
-	$(function() {
-		$addBtn.on('click', onAddBtnClick);
-		renderTasksList();
-	});
+  function setSaveAction(saveButton, editInput, index, editBlock) {
+    saveButton.on('click', function() {
+      tasksArray[index] = editInput.val();
+      renderTasksList();
+      editBlock.remove();
+    })
+  }
+
+  $(function() {
+    $addBtn.on('click', onAddBtnClick);
+    renderTasksList();
+});
 })(jQuery);
