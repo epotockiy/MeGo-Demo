3950cca5adfe00301c29e554f31a2032dd9b0f26
nsasin <nikitasasin@gmail.com>
2017-07-21 12:59:55 +0300

    Add the localstorage. Changed a script logic

--- Task3/src/js/main.js
+++ Task3/src/js/main.js
@@ -1,82 +1,197 @@
-$(document).ready(function(){
-	//ADD ITEM
-	$('button#add').on('click',function(){
-		//get input's value
-		var $newTask = $('#new-task').val();
-		//check for empty value
-		if($newTask==='') {
-			//show alert message
-			$('.warning').html('No task added').show();
-			//hide success message
-			$('.success').hide();
-		}else {
-			//show success message
-			$('.success').html('Task added to list').fadeIn('slow').delay(500).fadeOut();
-			//hide warning
-			$('.warning').hide();
-			//generate and add new list item
-			var newListItem = '<li>';
-			newListItem+='<input type="checkbox">';
-			newListItem+='<label>'+$newTask+'</label>';
-			newListItem+='<input type="text" class="inputTask">';
-			newListItem+='<button class="edit">Edit</button>';
-			newListItem+='<button class="delete">Delete</button>';
-			newListItem+='</li>';
-      		//apend to list
-			$('ul#incomplete-tasks').append(newListItem);
-      		$('.inputTask').val($newTask);
-			//empty input value
-			$('#new-task').val('');
-		};//end else statement
-		countTask();
-	});//end button click function
+function todoList() {
+  var taskLists = {};
+  var tasksStorage = storage('tasksList');
 
-	//EDIT TASK
-	//user clicks on edit button
-	$('ul').on('click','.edit',function(){
-		//get parent
-		var parent = $(this).parent();
-		//check parent class
-		if (!parent.hasClass('editMode')) {
-			parent.addClass('editMode');
-		}else if (parent.hasClass('editMode')) {	
-			//grab value entered in input and set as label
-			var editTask = $(this).prev('input[type="text"]').val();
-			var editLabel = parent.find('label');
-			editLabel.html(editTask);
-			//remove edit class
-			parent.removeClass('editMode');
-		};
-	});
+	function init() {
+    var initialListsItems = tasksStorage.get();
+    taskLists = renderTaskLists(initialListsItems);
+    watchAddTaskForm();
 
-	//COMPLETE TASK
-	$('ul').on('change','input[type="checkbox"]', function(){
-		//get grand parent
-		var grandpa = $(this).parent().parent();
-		//get parent
-		var parent = $(this).parent();
-		//check which ul the li is under
-		if (grandpa.is('#incomplete-tasks')) {
-			parent.remove();
-			$('#completed-tasks').append(parent);	
-		}else if(grandpa.is('#completed-tasks')){
-			parent.remove();
-			$('#incomplete-tasks').append(parent);
-		}
-		countTask();
-	});
+    return taskLists;
+	}
 	
-	//DELETE TASK
-	$('ul').on('click','.delete',function(){
-		$(this).parent().remove();
-		countTask();
-	});//end delete function
+	function watchAddTaskForm() {
+    	var addTaskInput = $('#new-task'),
+		addTaskSubmitBtn = $('#add');
 
-	//TASK COUNTER
-	function countTask(){
-		var remainTask = $('#incomplete-tasks li').length;
-		$('#counter').hide().fadeIn(300).html(remainTask);
-	};
-	countTask();
+		addTaskSubmitBtn.on('click', function () {
+			var item = {
+				id: Math.random().toString(36).substr(2, 9), // генерация уникального id
+        		value: addTaskInput.val(),
+        		completed: false
+      		};
+
+      		addItemToList(item);
+      		tasksStorage.setItem(item);
+    	});
+
+		function addItemToList(item) {
+      		taskLists.incompleted.append(
+        		renderListItem(item)
+			)
+		}
+  }
+
+	function renderTaskLists(initialListsItems) {
+		var completedList = $('#completed-tasks'),
+      	incompletedList = $('#incomplete-tasks');
+
+    initialListsItems.forEach(function (item) {
+			if (item.completed) {
+				return completedList.append(
+					renderListItem(item)
+				)
+			}
+
+      return incompletedList.append(
+        renderListItem(item)
+      )
+    });
+
+    return {
+			incompleted: incompletedList,
+      completed: completedList
+		}
+  	}
+	
+	function renderListItem(item) {
+		var taskItem = $('<li/>', {
+					'data-id': item.id
+				}),
+				taskText = $('<label/>', {
+					text: item.value
+				}),
+				taskCheckbox = $('<input/>', {
+					type: 'checkbox',
+					value: item.id,
+					checked: item.completed
+				}),
+				taskInput = $('<input/>', {
+					type: 'text',
+					value: item.value
+				}),
+				taskEditBtn = $('<button/>', {
+					text: 'Edit',
+					class: 'edit'
+				}),
+				taskDeleteBtn = $('<button/>', {
+					text: 'Delete',
+					class: 'delete'
+				});
+
+		taskCheckbox.on('change', moveItem);
+
+    taskEditBtn.on('click', editItem);
+
+    taskDeleteBtn.on('click', deleteItem);
+
+    function deleteItem() {
+      var completed = taskCheckbox.prop('checked'),
+					id = taskCheckbox.val();
+
+      if (completed) {
+        taskLists.completed.find(taskItem).remove();
+			} else {
+        taskLists.incompleted.find(taskItem).remove();
+      }
+
+      tasksStorage.removeItem(id);
+    }
+
+    function editItem() {
+      var id = taskItem.attr('id'),
+        	editModeClass = 'editMode';
+
+      if (taskItem.hasClass(editModeClass)) {
+        var newValue = taskInput.val();
+        taskText.text(newValue);
+        tasksStorage.setItemValue(id, 'value', newValue);
+      }
+
+      taskItem.toggleClass(editModeClass);
+    }
+
+    function moveItem() {
+      var id = $(this).val(),
+        completed = $(this).prop('checked');
+
+      if (completed) {
+        taskLists.incompleted.find('[data-id=' + id + ']').appendTo(taskLists.completed);
+      } else {
+        taskLists.completed.find('[data-id=' + id + ']').appendTo(taskLists.incompleted);
+      }
+      tasksStorage.setItemValue(id, 'completed', completed);
+    }
+
+    return appendChildrenArray(taskItem, [
+      taskCheckbox,
+      taskText,
+      taskInput,
+      taskEditBtn,
+			taskDeleteBtn
+		])
+  }
+
+	return {
+		init: init
+	}
+}
+
+function storage(path){
+  function setItemValue(id, field, value) {
+    var currentItems = readDataFromStorage();
+
+    currentItems.forEach(function (item) {
+      if (item.id === id) {
+        item[field] = value
+      }
+    });
+
+    saveArray(currentItems);
+  }
+
+  function saveArray(array) {
+    localStorage.setItem(path, JSON.stringify(array));
+  }
+
+  function saveItem(item) {
+    var currentItems = readDataFromStorage();
+    currentItems.push(item);
+    saveArray(currentItems)
+  }
+
+  function removeItem(id) {
+    var currentItems = readDataFromStorage();
+    saveArray(
+      currentItems.filter(function (item) {
+        return item.id !== id;
+      })
+		);
+  }
+
+  function readDataFromStorage() {
+    return JSON.parse(localStorage.getItem(path)) || [];
+  }
+
+  return {
+    setItem: function (item) { saveItem(item); },
+    setItemValue: setItemValue,
+    setArray: saveArray,
+    removeItem: removeItem,
+    get: readDataFromStorage
+  }
+}
+
+function appendChildrenArray(parent, children) {
+  children.forEach(function (item) {
+    parent.append(item);
+  });
+
+  return parent;
+}
+
+$(document).ready(function(){
+
+  var taskLists = todoList().init();
 
 });
