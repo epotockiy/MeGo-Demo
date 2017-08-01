f79c6525e057128ca43cffd68ac1a2bfff605621
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-08-01 10:14:57 +0300

    Added build to task4 app.

--- task5/src/app/components/Todo.js
+++ task5/src/app/components/Todo.js
@@ -4,9 +4,18 @@ export class Todo extends React.Component {
   isStorageAvailable = false;
   tasksArray = [];
 
+  constructor() {
+    super();
+
+    this.state = {
+      taskInput: '',
+      editInput: ''
+    };
+  }
+
   getTasksFromStorage() {
     if (typeof localStorage !== 'undefined') {
-      this.tasksArray = localStorage.getItem('TasksArray' + this.props.id) || [];
+      this.tasksArray = JSON.parse(localStorage.getItem('tasksArray' + this.props.id)) || [];
       this.isStorageAvailable = true;
     } else {
       this.tasksArray = [];
@@ -18,18 +27,28 @@ export class Todo extends React.Component {
     localStorage.setItem('tasksArray' + this.props.id, JSON.stringify(this.tasksArray));
   }
 
-  // addNewTask(form) {
-  //   this.tasksArray.unshift({
-  //     name: form.taskName,
-  //     done: false
-  //   });
-  //
-  //   if (this.isStorageAvailable) {
-  //     this.saveListToStorage();
-  //   }
-  //
-  //   this.todoForm.reset();
-  // }
+  addNewTask(event) {
+    event.preventDefault();
+
+    this.tasksArray.unshift({
+      name: this.state.taskInput,
+      done: false
+    });
+
+    if (this.isStorageAvailable) {
+      this.saveListToStorage();
+    }
+
+    this.setState({
+      taskInput: ''
+    });
+  }
+
+  handleTaskInputChange(event) {
+    this.setState({
+      taskInput: event.target.value
+    });
+  }
   //
   // onDoneClick(index: number) {
   //   this.tasksArray[index].done = !this.tasksArray[index].done;
@@ -83,13 +102,13 @@ export class Todo extends React.Component {
 
     return (
         <div className="todo">
-          <form>
+          <form ref="task_form" onSubmit={this.addNewTask.bind(this)}>
             <input type="text"
                    className="task-input"
-                   placeholder="Enter task name..." />
-            <button type="submit" className="add-task-btn">
-              Adds
-            </button>
+                   placeholder="Enter task name..."
+                   value={this.state.taskInput}
+                   onChange={this.handleTaskInputChange.bind(this)}/>
+            <button type="submit" className="add-task-btn">Adds</button>
           </form>
 
           <div className="todo-list">
@@ -100,6 +119,10 @@ export class Todo extends React.Component {
               <button className="progress-filter">Progress</button>
               <button className="done-filter">Done</button>
             </div>
+
+            <ul>
+
+            </ul>
           </div>
         </div>
     );
