674113993570ba7e10cd545dbae319214d1f1082
CalvinStar <vladislavgriga@gmail.com>
2017-08-08 10:48:17 +0300

    Base html + styles without responsive design

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+
+gulp.task('sass', function() {
+  return gulp.src('./app/sass/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./app/css'));
+});
+
+
+// gulp.task('hel' , function() {
+// 	console.log("hello");
+// });
+
+
+// gulp.task('default' , ['hel']);
