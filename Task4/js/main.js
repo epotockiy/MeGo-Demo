051bc375a00e577321e0e491d8e216709147c1ad
nsasin <nikitasasin@gmail.com>
2017-08-10 16:50:51 +0300

    Fixed js slider

--- Task4/js/main.js
+++ Task4/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+;(function($, undefined) {
 	function slider() {
 		
 		var sliders = $("#slider");
@@ -25,6 +25,7 @@
 
 			for (var j = 0; j < centerSliderItems.length; j++) {
 				$(centerSliderItems[j]).attr("data-id", j+1);
+				$(rightSliderItems[j]).attr("data-id", j+1);
 			}
 
 			nav.on("click", function(event) {
@@ -36,38 +37,83 @@
 				var target = $(event.target);
 				moveRight(centerSlider, target, rightSliderWidth);
 			});
-		}
 
-		function moveCenter(nav, clicked, sliderElems) {
-			var clickedPosition = clicked.attr("data-id");
-			var activeElem = nav.find("li.active");
-			var activePosition = activeElem.attr("data-id");
-			var slider = sliders.find(".center-slider ul");
-			var position = parseInt(slider.css("margin-top"));
+			function moveCenter(nav, clicked, sliderElems) {
+				var clickedPosition = clicked.attr("data-id");
+				var activeElem = nav.find("li.active");
+				var activePosition = activeElem.attr("data-id");
+				var slider = sliders.find(".center-slider ul");
+				var activeSliderElem = slider.find("li.active");
+				var newActiveSliderPos = 1 + (sliderElems.inColumn * (clickedPosition - 1));
+				var newActiveSliderElem = slider.find("li[data-id=" + newActiveSliderPos + "]");
+				var position = parseInt(slider.css("margin-top"));
 
-			if(sliderElems.scroll != 1 && sliderElems.scroll >= clickedPosition) {
+				if(sliderElems.scroll != 1 && sliderElems.scroll >= clickedPosition) {
+					moveRight(centerSlider, newActiveSliderElem, rightSliderWidth);
+					
+					activeSliderElem.removeClass("active");
+					newActiveSliderElem.addClass("active");
+					var moveCount = clickedPosition - activePosition;
+					console.log(moveCount + ' ' + sliderElems.inColumn + " " + sliderElems.height);
+					slider.css({
+						"margin-top": position - (moveCount * sliderElems.inColumn * sliderElems.height)
+					});
+					activeElem.removeClass("active");
+					clicked.addClass("active");
+
+				}
+			}
+
+			function moveRight(centerSlider, clicked, rightSliderWidth) {
+				var clickedPosition = clicked.attr("data-id");
+				var activeElem = centerSlider.find("li.active");
+				var activePosition = activeElem.attr("data-id");
+				var slider = sliders.find(".right-slider ul");
+				var activeSliderElem = slider.find("li.active");
+				var newActiveSliderElem = slider.find("li[data-id=" + clickedPosition + "]");
+				var position = parseInt(slider.css("margin-left"));
 				var moveCount = clickedPosition - activePosition;
-				slider.css({
-					"margin-top": position - (moveCount * sliderElems.inColumn * sliderElems.height)
-				});
+				
+				newActiveSliderElem.addClass("active");
+
+				if (moveCount > 0 && clickedPosition !== activePosition) {
+					newActiveSliderElem.css({
+						"margin-left": rightSliderWidth.width
+					}).stop()
+			          .animate({
+			            'margin-left': 0
+			          }, 300, function() {
+			          	activeSliderElem.removeClass("active");
+			          });
+
+			          activeSliderElem.css({
+						"margin-left": 0
+					}).stop()
+			          .animate({
+			            'margin-left': -rightSliderWidth.width
+			          }, 300);
+				}
+				else if(clickedPosition !== activePosition) {
+					newActiveSliderElem.css({
+						"margin-left": -rightSliderWidth.width
+					}).stop()
+			          .animate({
+			            'margin-left': 0
+			          }, 300, function() {
+			          	activeSliderElem.removeClass("active");
+			          });
+
+			        activeSliderElem.css({
+						"margin-left": 0
+					}).stop()
+			          .animate({
+			            'margin-left': rightSliderWidth.width
+			          }, 300);
+				}
+
 				activeElem.removeClass("active");
 				clicked.addClass("active");
 			}
-		}
-
-		function moveRight(centerSlider, clicked, rightSliderWidth) {
-			var clickedPosition = clicked.attr("data-id");
-			var activeElem = centerSlider.find("li.active");
-			var activePosition = activeElem.attr("data-id");
-			var slider = sliders.find(".right-slider ul");
-			var position = parseInt(slider.css("margin-left"));
-			console.log()
-			var moveCount = clickedPosition - activePosition;
-			slider.css({
-				"margin-left": position - (moveCount * rightSliderWidth.width)
-			});
-			activeElem.removeClass("active");
-			clicked.addClass("active");
 		}
 
 		function countCenterSLiderScroll(sliders) {
@@ -89,13 +135,16 @@
 			var list = sliders.find(".right-slider ul");
 			var elems = list.find("li");
 			var elemWidth = list.width();
-			
-			list.width(elems.length * elemWidth);
-			$.each(elems, function(i, item){
-				return $(item).width(elemWidth);
+
+			list.css({
+				"width" : elemWidth
+			});
+			elems.css({
+				"width" : elemWidth,
+				"height" : list.height()
 			});
 
-			var width = parseInt(elems.first().css("width"));
+			var width = elems.first().width();
 
 			return {
 				width: width
@@ -105,10 +154,29 @@
 		return {
 			init: init
 		};
-
 	}
 
 	$(document).ready(function() {
-		var a = slider().init();
+		slider().init();
+		$("#slider").hide();
+
+	    $("header").find('li').on('click', function() {
+	    	switch($(this).attr('id')) {
+
+		        case "nav-js":
+		        	$("#slider").show();
+		        	$("#css-slider").hide();
+		          	break;
+
+		        case "nav-css":
+		          	$("#slider").hide();
+		        	$("#css-slider").show();
+		        	break;
+
+		        default:
+		          	console.log("Error!");
+		          	break;
+	    	}
+	    });		
   	});
-})();
+})(jQuery);
