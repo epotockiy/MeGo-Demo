c5d4c54038102bb831f1b03a4b181bcd7c0f9401
CalvinStar <vladislavgriga@gmail.com>
2017-08-10 17:44:30 +0300

    Some our work - last box

---
+++ app/js/accordeon.js
@@ -0,0 +1,21 @@
+
+window.onload = function () {
+	var title = document.getElementsByClassName('accordion-title');
+	var content = document.getElementsByClassName('accordion-content');
+	var b = 0;
+	for( var i=0; i < title.length; i++) {
+		title[i].addEventListener('click' , function() {
+			console.log(b++);
+			if(!(this.classList.contains('active'))) {
+				for( var i=0; i < title.length; i++){
+					title[i].classList.remove('active');
+					console.log("remove");
+				}
+				this.classList.add('active');
+			}
+		})
+	}
+}
+
+
+
