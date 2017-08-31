d642a7d4673870c8348dbc5fe9d0dba947c089b1
Eugene Motolov <zheniamotolov@gmail.com>
2017-08-31 15:55:25 +0300

    fixed bugs with image animation

--- task_4_slider/src/js/Slider.js
+++ task_4_slider/src/js/Slider.js
@@ -1,15 +1,14 @@
-
 var Slider = (function () {
     function Slider(baseElement) {
-        this.baseElement=$(baseElement);
+        this.baseElement = $(baseElement);
         this.submenuContainer = this.baseElement.find('.slider-submenu-container');
-        // this.sliderContainerImage = $('.image-container');
         this.sliderImage = this.baseElement.find('.slider-image');
+        this.sliderImageContainer = this.baseElement.find('.image-container');
         this.sliderSubMenu = this.baseElement.find('.slider-submenu');
         this.sliderMenu = this.baseElement.find('.slider-menu');
+        this.preventSecondaryAnimation = null;
         this.sliderInitializer()
     }
-
     Slider.prototype.sliderInitializer = function () {
         this.menuEventHandler();
         this.subMenuEventHandler();
@@ -20,36 +19,42 @@ var Slider = (function () {
             var target = e.target;
             var dataIdOfMenuElement = target.getAttribute('data-menuItemId');
             if (dataIdOfMenuElement) {
+                this.preventSecondaryAnimation = dataIdOfMenuElement * 3;
                 self.slideTo(dataIdOfMenuElement);
             }
         })
     };
     Slider.prototype.subMenuEventHandler = function () {
         var self = this;
-        var preventSecondaryAnimation = null;
         this.sliderSubMenu.click(function (e) {
             var target = e.target;
             var dataIdOfSubMenuElement = target.getAttribute('data-subMenuItemId');
-            if (dataIdOfSubMenuElement && dataIdOfSubMenuElement !== preventSecondaryAnimation) {
-                preventSecondaryAnimation = dataIdOfSubMenuElement;
+            if (dataIdOfSubMenuElement) {
                 self.slideImage(dataIdOfSubMenuElement);
             }
         })
     };
     Slider.prototype.slideImage = function (imageNumber) {
-        this.sliderImage.animate({opacity: 0}, 'slow', function () {
-            $(this)
-                .css('background-image', "url('img/img" + imageNumber + ".png')")
-                .animate({opacity: 1});
-        });
-
+        if (this.preventSecondaryAnimation !== Number(imageNumber)) {
+            this.preventSecondaryAnimation = imageNumber;
+            var newSliderImage = $('<div class="slider-image"></div>');
+            var self = this;
+            this.sliderImageContainer.append(newSliderImage);
+            newSliderImage
+                .css({'background-image': "url('img/img" + imageNumber + ".png')", 'left': '430px'})
+                .animate({left: '0'}, 400, function () {
+                    self.sliderImageContainer.children().first().remove()
+                });
+        }
+        else {
+            return;
+        }
     };
     Slider.prototype.slideTo = function (dataIdOfMenuElement) {
         this.submenuContainer.animate({top: -dataIdOfMenuElement * 3 * 45 + 'px'}, "slow");
+        this.slideImage(dataIdOfMenuElement * 3);
     };
-    // Slider.prototype.subMenuSlideAnimation = function () {
-    //
-    // };
     return Slider;
-})();
+})
+();
 export {Slider}
